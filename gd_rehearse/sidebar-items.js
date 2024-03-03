68db605234f24e9037dc0d00ccfbf8f4d57398fe
StatisMike <StatisMike@users.noreply.github.com>
2024-03-03 19:56:03 +0000

    deploy: 9e05d68bf98c2175cc321b1b63c3fe6707032827

---
+++ gd_rehearse/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["bench","itest"],"struct":["GdTestRunner"]};
---
+++ trait.impl/gd_rehearse_defs/cases/trait.CaseContext.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"gd_rehearse":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
