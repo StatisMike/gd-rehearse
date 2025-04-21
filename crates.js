b421e3d04b5d4fb0b87aafedc575a2376dad82ec
StatisMike <StatisMike@users.noreply.github.com>
2025-04-21 08:14:23 +0000

    deploy: e1300549e52fea3dae7f3312047864c10d5a72bb

---
+++ crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["gd_rehearse"];
+//{"start":21,"fragment_lengths":[13]}
---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["gd_rehearse",["",[],["lib.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[34]}
